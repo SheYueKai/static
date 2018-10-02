5d589fe648f7d57365722c55ed85c7839cfaa975
kay <978398138@qq.com>
2018-10-02 13:11:17 +0800

    variable

--- origin/js/interview/variable.js
+++ origin/js/interview/variable.js
@@ -31,3 +31,26 @@ function search(str,key){
 console.log(search(string,'L'));
 console.log(search(string,'e'));
 console.log(search(string,'M'));
+
+var box = '//lee李';
+console.log(encodeURI(box));
+console.log(encodeURIComponent(box));
+console.log(decodeURI(encodeURI(box)));
+console.log(decodeURIComponent(encodeURIComponent(box)));
+
+console.log(Math.min(1,2,3,4,5,6,7,8,9));
+console.log(Math.max(1,2,3,4,5,6,7,8,9));
+
+console.log("Math.round(25.1) = " + Math.round(25.1));//四舍五入
+console.log("Math.round(25.5) = " + Math.round(25.5));
+console.log("Math.ceil(25.1) = " + Math.ceil(25.1));//向上取整
+console.log("Math.floor(25.9) = " + Math.floor(25.9));//向下取整
+
+function random(lower,upper){
+    var num = Math.abs(upper - lower) + 1;
+    return Math.floor(Math.random() * num + Math.min(lower,upper));
+}
+for(var i = 0;i < 10; i++){
+    // console.log(i + ". random = " + random(10,1));
+    console.log(i + ". random = " + random(1,10));
+}
