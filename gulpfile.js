d35f15c2449d8fb7a945d515060556585777dfea
kay <978398138@qq.com>
2018-10-06 00:13:45 +0800

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
 var gulp = require('gulp')
+
 var less = require('gulp-less')
 var lessSrc = 'origin/less/**/*.less',
     jsSrc = 'origin/js/**/*.js',
     lessDest = 'htdocs/res/css',
     jsDest = 'htdocs/res/js';
+var babel = require('gulp-babel')
+var plumber = require('gulp-plumber')
 
 gulp.task('less', function(){
     gulp.src(lessSrc)
+    .pipe(plumber())
     .pipe(less())
     .pipe(gulp.dest(lessDest))
 });
 
 gulp.task('js', function(){
     gulp.src(jsSrc)
+    .pipe(plumber())
+    // .pipe(babel())
     .pipe(gulp.dest(jsDest))
 });
 
